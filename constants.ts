f74ef1b9e097b28a7fb0705c9b896046f4353f17
RJ.P <329rpodlas@frhsd.com>
2026-04-08 21:42:42 -0300

    Add files via upload

--- constants.ts
+++ constants.ts
@@ -881,7 +881,7 @@ export const PARTNERS_DATA: PartnerItem[] = [
 
 export const STAFF_DATA: StaffMember[] = [
     { name: "RJ.P", role: "Owner", img: "https://files.catbox.moe/5mijpj.png", link: "https://guns.lol/zzemie" },
-    { name: "pcidiagnosticbus", role: "Co-Owner", img: "https://files.catbox.moe/rgh3mz.png", }
+    { name: "ANDREJ", role: "Co-Owner", img: "https://files.catbox.moe/rgh3mz.png", }
 ];
 
 
